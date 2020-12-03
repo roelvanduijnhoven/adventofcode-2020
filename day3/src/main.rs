a53219af73f69e977bfb4c9dca5a51cd701eca7b
Roel van Duijnhoven <roel@vanduijnhoven.com>
2020-12-03 07:58:41 +0100

    Solve day 3.

---
+++ day3/src/main.rs
@@ -0,0 +1,52 @@
+mod puzzle;
+
+use puzzle::Puzzle;
+use std::fs;
+
+fn read_puzzle_from_file(input_path: String) -> Puzzle {
+    let content = fs::read_to_string(input_path).expect("Something went wrong reading the file");
+    return Puzzle::read_from_string(&content);
+}
+
+fn get_tree_encounters(puzzle: Puzzle, x_step: usize, y_step: usize ) -> usize {
+    let mut x = 0; 
+    let mut y = 0;
+
+    let mut trees_encountered = 0;
+
+    loop {
+        x += x_step;
+        y += y_step;
+
+        if y >= puzzle.height {
+            break;
+        }
+
+        if puzzle.is_tree(x, y) {
+            trees_encountered += 1;
+        }
+    }
+        
+    return trees_encountered;
+}
+
+fn main() {
+    // TODO How not to copy the reading part
+
+    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
+    let right_1_down_1 = get_tree_encounters(puzzle, 1, 1);
+
+    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
+    let right_3_down_1 = get_tree_encounters(puzzle, 3, 1);
+
+    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
+    let right_5_down_1 = get_tree_encounters(puzzle, 5, 1);
+
+    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
+    let right_7_down_1 = get_tree_encounters(puzzle, 7, 1);
+
+    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
+    let right_1_down_2 = get_tree_encounters(puzzle, 1, 2);
+
+    println!("{}", right_1_down_1 * right_1_down_2 * right_3_down_1 * right_5_down_1 * right_7_down_1);
+}
---
+++ day3/src/puzzle.rs
@@ -0,0 +1,64 @@
+#[derive(Debug)]
+pub struct Puzzle {
+    pub width: usize,
+    pub height: usize,
+    trees: Vec<bool>,
+}
+
+impl Puzzle {
+    pub fn is_tree(&self, x: usize, y: usize) -> bool {
+        let wrapped_x = x % self.width;
+        let position = y * self.width + wrapped_x;
+        return self.trees[position];
+    }
+
+    pub fn read_from_string(input: &str) -> Puzzle {
+        let mut width = 0;
+        let mut height = 0;
+        let mut trees = vec![];
+        for line in input.split("\n") {
+            width = 0;
+            for character in line.chars() {
+                trees.push(character == '#');
+                width += 1;
+            }
+            height += 1;
+        }
+        Puzzle { width: width, height: height, trees: trees }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_will_detect_normal_positions() {
+        let puzzle = Puzzle::read_from_string("..#..\n####.");
+        assert_eq!(false, puzzle.is_tree(0, 0));
+        assert_eq!(false, puzzle.is_tree(1, 0));
+        assert_eq!(true, puzzle.is_tree(2, 0));
+        assert_eq!(false, puzzle.is_tree(3, 0));
+        assert_eq!(false, puzzle.is_tree(4, 0));
+        assert_eq!(true, puzzle.is_tree(0, 1));
+        assert_eq!(true, puzzle.is_tree(1, 1));
+        assert_eq!(true, puzzle.is_tree(2, 1));
+        assert_eq!(true, puzzle.is_tree(3, 1));
+        assert_eq!(false, puzzle.is_tree(4, 1));
+    }
+
+    #[test]
+    fn it_will_wrap_on_columns() {
+        let puzzle = Puzzle::read_from_string("..#..\n####.");
+        assert_eq!(false, puzzle.is_tree(5, 0));
+        assert_eq!(false, puzzle.is_tree(11, 0));
+        assert_eq!(true, puzzle.is_tree(7, 0));
+        assert_eq!(false, puzzle.is_tree(18, 0));
+        assert_eq!(false, puzzle.is_tree(9, 0));
+        assert_eq!(true, puzzle.is_tree(50, 1));
+        assert_eq!(true, puzzle.is_tree(51, 1));
+        assert_eq!(true, puzzle.is_tree(22, 1));
+        assert_eq!(true, puzzle.is_tree(23, 1));
+        assert_eq!(false, puzzle.is_tree(9, 1));       
+    }    
+}
