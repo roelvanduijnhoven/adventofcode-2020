2a646e6d832da9b7474d23792fb4208460d20b0d
Roel van Duijnhoven <roel@vanduijnhoven.com>
2020-12-03 08:03:15 +0100

    Borrow puzzle input.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -8,7 +8,7 @@ fn read_puzzle_from_file(input_path: String) -> Puzzle {
     return Puzzle::read_from_string(&content);
 }
 
-fn get_tree_encounters(puzzle: Puzzle, x_step: usize, y_step: usize ) -> usize {
+fn get_tree_encounters(puzzle: &Puzzle, x_step: usize, y_step: usize ) -> usize {
     let mut x = 0; 
     let mut y = 0;
 
@@ -31,22 +31,17 @@ fn get_tree_encounters(puzzle: Puzzle, x_step: usize, y_step: usize ) -> usize {
 }
 
 fn main() {
-    // TODO How not to copy the reading part
-
     let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
-    let right_1_down_1 = get_tree_encounters(puzzle, 1, 1);
 
-    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
-    let right_3_down_1 = get_tree_encounters(puzzle, 3, 1);
+    let right_1_down_1 = get_tree_encounters(&puzzle, 1, 1);
+    let right_3_down_1 = get_tree_encounters(&puzzle, 3, 1);
+    let right_5_down_1 = get_tree_encounters(&puzzle, 5, 1);
+    let right_7_down_1 = get_tree_encounters(&puzzle, 7, 1);
+    let right_1_down_2 = get_tree_encounters(&puzzle, 1, 2);
 
-    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
-    let right_5_down_1 = get_tree_encounters(puzzle, 5, 1);
-
-    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
-    let right_7_down_1 = get_tree_encounters(puzzle, 7, 1);
-
-    let puzzle = read_puzzle_from_file("assets/day3.input".to_string());
-    let right_1_down_2 = get_tree_encounters(puzzle, 1, 2);
-
-    println!("{}", right_1_down_1 * right_1_down_2 * right_3_down_1 * right_5_down_1 * right_7_down_1);
+    println!(
+        "{} x {} x {} x {} x {} = {}",
+        right_1_down_1, right_1_down_2, right_3_down_1, right_5_down_1, right_7_down_1,
+        right_1_down_1 * right_1_down_2 * right_3_down_1 * right_5_down_1 * right_7_down_1
+    );
 }
