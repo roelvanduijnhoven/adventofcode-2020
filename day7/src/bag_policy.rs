6255d766792f0d1ee689e0402c15bae6d070e8af
Roel van Duijnhoven <roel@vanduijnhoven.com>
2020-12-07 19:07:51 +0100

    Implement part two.

--- day7/src/bag_policy.rs
+++ day7/src/bag_policy.rs
@@ -1,16 +1,47 @@
+use regex::Regex;
 use std::collections::HashMap;
 
 pub struct BagPolicy {
     bags_in_system: Vec<String>,
+
+    // Maps bag to all bags that can contain it.
     containment_map: HashMap<String, Vec<String>>,
+
+    // Maps a container bag to all things it contains.
+    container_map: HashMap<String, Vec<(String, usize)>>,
 }
 
 impl BagPolicy {
-    pub fn new () -> BagPolicy {
-        BagPolicy { containment_map: HashMap::new(), bags_in_system: vec![] }
+    pub fn new() -> BagPolicy {
+        BagPolicy { 
+            containment_map: HashMap::new(),
+            bags_in_system: vec![],
+            container_map: HashMap::new(),
+        }
     }
 
-    pub fn learn(&mut self, target: &str, container: &str) {
+    pub fn from_string(content: &str) -> BagPolicy {
+        let mut policy = BagPolicy::new();
+        for line in content.split("\n") {
+            let re = Regex::new(r"^(.+) bags contain (.+)\.$").unwrap();
+            let result = re.captures(&line).unwrap();
+            let container: &str = &result[1];
+    
+            if &result[2] == "no other bags" {
+                continue;
+            }
+    
+            let targets: Vec<&str> = result[2].split(", ").collect();
+            for target in targets {
+                let re = Regex::new(r"^([0-9]+) (.+) (bag|bags)$").unwrap();
+                let result = re.captures(&target).unwrap();
+                policy.learn(container, &result[2], result[1].parse::<usize>().unwrap());
+            }
+        }
+        policy
+    }
+
+    pub fn learn(&mut self, container: &str, target: &str, amount: usize) {
         if !self.bags_in_system.contains(&target.to_string()) {
             self.bags_in_system.push(target.to_string());
         }
@@ -27,13 +58,22 @@ impl BagPolicy {
             .get_mut(&String::from(target))
             .unwrap()
             .push(container.to_string());
+
+        self.container_map
+            .entry(String::from(container))
+            .or_insert(vec![]);
+
+        self.container_map
+            .get_mut(&String::from(container))
+            .unwrap()
+            .push((target.to_string(), amount));
     }
 
     pub fn get_bags(&self) -> &Vec<String> {
         &self.bags_in_system
     }
 
-    pub fn can_contain(&self, target: &str, container: &str) -> bool {
+    pub fn is_contained_in(&self, target: &str, container: &str) -> bool {
         if target == container {
             return false;
         }
@@ -57,17 +97,28 @@ impl BagPolicy {
                 return true;
             }
 
-            let can_contain = match self.containment_map.get(fits_in_target) {
+            let is_contained_in = match self.containment_map.get(fits_in_target) {
                 None => continue,
                 Some(value) => value,
             };
 
-            for new_target in can_contain {
+            for new_target in is_contained_in {
                 queue.push(new_target);
             }
         }
 
         false
+    }
+
+    pub fn count_bags(&self, bag: &str) -> usize {
+        match self.container_map.get(bag) {
+            None => 0,
+            Some(bags) => 
+                bags
+                    .iter()
+                    .map(|(child_bag, count)| count + count * self.count_bags(&child_bag))
+                    .sum()
+        }
     }
 }
 
@@ -77,42 +128,52 @@ mod tests {
 
     #[test]
     fn it_will_store() {
-        let mut policy = BagPolicy::new();
-        policy.learn("shiny gold", "bright white");
-        policy.learn("shiny gold", "muted yellow");
-        policy.learn("muted yellow", "dark orange");
-        policy.learn("bright white", "light red");
-        policy.learn("muted yellow", "light red");
-
-        assert!(policy.can_contain("shiny gold", "bright white"));
-        assert!(policy.can_contain("shiny gold", "muted yellow"));
-        assert!(policy.can_contain("muted yellow", "dark orange"));
-        assert!(policy.can_contain("bright white", "light red"));
-        assert!(policy.can_contain("muted yellow", "light red"));
-
-        assert!(policy.can_contain("shiny gold", "bright white"));
-        assert!(policy.can_contain("shiny gold", "muted yellow"));
-        assert!(policy.can_contain("shiny gold", "dark orange"));
-        assert!(policy.can_contain("shiny gold", "light red"));
+        let policy = BagPolicy::from_string(
+"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags."
+        );
         
-        assert_eq!(false, policy.can_contain("shiny gold", "shiny gold"));
-        assert_eq!(false, policy.can_contain("shiny gold", "faded blue"));
-        assert_eq!(false, policy.can_contain("shiny gold", "dark olive"));
-        assert_eq!(false, policy.can_contain("shiny gold", "vibrant plum"));
-        assert_eq!(false, policy.can_contain("shiny gold", "dotted black"));
+        assert!(policy.is_contained_in("shiny gold", "bright white"));
+        assert!(policy.is_contained_in("shiny gold", "muted yellow"));
+        assert!(policy.is_contained_in("muted yellow", "dark orange"));
+        assert!(policy.is_contained_in("bright white", "light red"));
+        assert!(policy.is_contained_in("muted yellow", "light red"));
+
+        assert!(policy.is_contained_in("shiny gold", "bright white"));
+        assert!(policy.is_contained_in("shiny gold", "muted yellow"));
+        assert!(policy.is_contained_in("shiny gold", "dark orange"));
+        assert!(policy.is_contained_in("shiny gold", "light red"));
+        
+        assert_eq!(false, policy.is_contained_in("shiny gold", "shiny gold"));
+        assert_eq!(false, policy.is_contained_in("shiny gold", "faded blue"));
+        assert_eq!(false, policy.is_contained_in("shiny gold", "dark olive"));
+        assert_eq!(false, policy.is_contained_in("shiny gold", "vibrant plum"));
+        assert_eq!(false, policy.is_contained_in("shiny gold", "dotted black"));
+
+        assert_eq!(0, policy.count_bags("faded blue"));
+        assert_eq!(0, policy.count_bags("dotted black"));
+        assert_eq!(11, policy.count_bags("vibrant plum"));
+        assert_eq!(7, policy.count_bags("dark olive"));
     }
 
     #[test]
     fn it_will_detect_cycles() {
         let mut policy = BagPolicy::new();
-        policy.learn("shiny gold", "bright white");
-        policy.learn("bright white", "shiny gold");
+        policy.learn("shiny gold", "bright white", 1);
+        policy.learn("bright white", "shiny gold", 1);
         
-        assert!(policy.can_contain("shiny gold", "bright white"));
-        assert!(policy.can_contain("bright white", "shiny gold"));
-        assert_eq!(false, policy.can_contain("shiny gold", "huh"));
-        assert_eq!(false, policy.can_contain("bright white", "huh"));
-        assert_eq!(false, policy.can_contain("huh", "shiny gold"));
-        assert_eq!(false, policy.can_contain("huh", "bright white"));
+        assert!(policy.is_contained_in("shiny gold", "bright white"));
+        assert!(policy.is_contained_in("bright white", "shiny gold"));
+        assert_eq!(false, policy.is_contained_in("shiny gold", "huh"));
+        assert_eq!(false, policy.is_contained_in("bright white", "huh"));
+        assert_eq!(false, policy.is_contained_in("huh", "shiny gold"));
+        assert_eq!(false, policy.is_contained_in("huh", "bright white"));
     }
 }
--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,34 +2,22 @@ mod bag_policy;
 
 use bag_policy::BagPolicy;
 use std::fs;
-use regex::Regex;
 
 fn main() {
-    let mut policy = BagPolicy::new();
-
     let content = fs::read_to_string("assets/day7.in").expect("Something went wrong reading the file");
-    for line in content.split("\n") {
-        let re = Regex::new(r"^(.+) bags contain (.+)\.$").unwrap();
-        let result = re.captures(&line).unwrap();
-        let container: &str = &result[1];
+    let policy = BagPolicy::from_string(&content);
 
-        if &result[2] == "no other bags" {
-            continue;
-        }
-
-        let targets: Vec<&str> = result[2].split(", ").collect();
-        for target in targets {
-            let re = Regex::new(r"^([0-9]+) (.+) (bag|bags)$").unwrap();
-            let result = re.captures(&target).unwrap();
-            policy.learn(&result[2], container);
-        }
+    {
+        let bags = policy.get_bags();
+        let can_contain_shiny_gold: Vec<&String> = bags
+            .iter()
+            .filter(|bag| policy.is_contained_in("shiny gold", bag))
+            .collect();    
+        
+        println!("Shiny gold bag can be contained in {} unique bags directly", can_contain_shiny_gold.len());
     }
 
-    let bags = policy.get_bags();
-    let can_contain_shiny_gold: Vec<&String> = bags
-        .iter()
-        .filter(|bag| policy.can_contain("shiny gold", bag))
-        .collect();
-        
-    println!("Shiny gold bag can be contained in {} unique bags directly", can_contain_shiny_gold.len());
+    {
+        println!("In a Shiny gold bag fit {} bags.", policy.count_bags("shiny gold"));
+    }
 }
