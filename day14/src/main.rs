64b39b97a8bb012d46df4a1f2b1b08b216ba1b0a
Roel van Duijnhoven <roel@vanduijnhoven.com>
2020-12-14 09:23:58 +0100

    Solution for part 2.

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::collections::HashMap;
 use regex::Regex;
+use combinations::Combinations;
 
 #[derive(Debug)]
 enum Instruction {
@@ -8,23 +9,39 @@ enum Instruction {
     Write(u64, u64),
 }
 
-fn apply_mask(value: u64, mask: &str) -> u64 {
-    let mut masked = value;
-    for (position, character) in mask.chars().rev().enumerate() {
-        masked = match character {
-            '1' => masked | ((2 as usize).pow(position as u32) as u64),
-            '0' => masked & !((2 as usize).pow(position as u32) as u64),
-            _ => masked,
+fn apply_mask(value: u64, mask: &str) -> String {
+    let string_value = format!("{:036b}", value);
+    let mut masked = String::new();
+    for (position, character) in mask.chars().enumerate() {
+        match character {
+            '0' => masked.push_str(&string_value[position..position + 1]),
+            value => masked.push(value),
         }
     }
     masked
 }
 
+fn find_all(input: &Vec<usize>) -> Vec<Vec<usize>> {
+    let mut output = vec![];
+
+    output.push(vec![]);
+    output.push(input.clone());
+
+    for i in 1..input.len() {
+        let combinations: Vec<_> = Combinations::new(input.clone(), i).collect();
+        for combination in combinations {
+            output.push(combination);
+        }
+    }
+
+    output
+}
+
 fn main() {
-    let program = load_program_from_string(&"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0");
+    let program = load_program_from_string(&"mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1");
 
     let contents = fs::read_to_string("assets/day14.in").expect("Something went wrong reading the file");
     let program = load_program_from_string(&contents);
@@ -35,7 +52,26 @@ mem[8] = 0");
     for instruction in program {
         match instruction {
             Instruction::UpdateMask(new_mask) => mask = new_mask,
-            Instruction::Write(memory_address, value) => { memory.insert(memory_address, apply_mask(value, &mask)); },
+            Instruction::Write(memory_address, value) => { 
+                let decoder = apply_mask(memory_address, &mask);
+                let x_positions: Vec<usize> = decoder
+                    .chars().rev().enumerate()
+                    .filter(|(_, x)| *x == 'X')
+                    .map(|(pos, _)| pos)
+                    .collect();
+
+                let base_value = u64::from_str_radix(&decoder.replace("X", "0"), 2).unwrap();
+
+                let combinations = find_all(&x_positions);
+                for combination in combinations {
+                    let mut memory_location = base_value;
+                    for position in combination {
+                        memory_location = memory_location | ((2 as u64).pow(position as u32) as u64);
+                    }
+
+                    memory.insert(memory_location, value);
+                }
+            },
         }
     }
 
