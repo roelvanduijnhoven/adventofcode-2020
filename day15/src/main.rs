f76ce8280ea8b106d4af4f73c54dcadd8ffd856f
Roel van Duijnhoven <roel@vanduijnhoven.com>
2020-12-15 07:53:46 +0100

    First part of day 15.

---
+++ day15/src/main.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+fn memory_game(input: &Vec<usize>, iterations: usize) -> usize {
+    let mut memory: HashMap<usize, (usize, usize)> = HashMap::new();
+    for (position, item) in input.iter().enumerate() {
+        memory.insert(*item, (position, position));
+    }
+
+    let mut previous = input[input.len() - 1];
+    for i in input.len()..iterations {
+        let (first, last) = memory.get(&previous).unwrap();
+        previous = last - first;
+
+        memory.insert(previous, match memory.get(&previous) {
+            None => (i, i),
+            Some((_, last)) => (*last, i),
+        });
+    }
+
+    previous
+}
+
+fn main() {
+    let input = vec![0,6,1,7,2,19,20];
+
+    println!("{}", memory_game(&input, 2020));
+}
