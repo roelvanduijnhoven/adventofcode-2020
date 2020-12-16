91d05ac81cabf04d4aff753e920c3c60ff0ce72e
Roel van Duijnhoven <roel@vanduijnhoven.com>
2020-12-16 09:35:30 +0100

    Part 2.

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -28,6 +28,9 @@ impl From<io::Error> for ImportError {
 fn read_input(filename: &str) -> Result<(Vec<Rule>, Ticket, Vec<Ticket>), ImportError> {
     let contents = fs::read_to_string(filename)?;
     let parts: Vec<&str> = contents.split("\n\n").collect();
+
+    // TODO Find out how to better map Option to my custom Error type, without duplicating the ok_or()'s and
+    // remove the .unwraps().
     
     // Part 1
     let mut rules = vec![];
@@ -71,20 +74,59 @@ fn parse_ticket(line: &str) -> Option<Ticket> {
     Some(Ticket { fields })
 }
 
+fn is_valid_ticket(ticket: &Ticket, rules: &Vec<Rule>) -> bool {
+    for field in &ticket.fields {
+        if !is_valid_field(*field, &rules) {
+            return false;
+        }
+    }
+    
+    true
+}
+
 fn is_valid_field(field: usize, rules: &Vec<Rule>) -> bool {
     for rule in rules {
-        for term in &rule.terms {
-            if field >= term.0 && field <= term.1 {
-                return true;
+        if validates_rule(field, &rule)    {
+            return true;
+        }
+    }
+    false
+}
+
+fn validates_rule(field: usize, rule: &Rule) -> bool {
+    for term in &rule.terms {
+        if field >= term.0 && field <= term.1 {
+            return true;
+        }
+    }
+    false
+}
+
+fn learn_rule(rule: &Rule, field_positions: Vec<usize>, tickets: &Vec<&Ticket>) -> Option<usize> {
+    let mut possible_positions = vec![];
+    for field_position in field_positions {
+        let mut is_possible = true;
+        for ticket in tickets {
+            if !validates_rule(ticket.fields[field_position], &rule) {
+                is_possible = false;
             }
+        }
+
+        if is_possible {
+            possible_positions.push(field_position);
         }
     }
 
-    false
+    if possible_positions.len() == 1 {
+        return Some(possible_positions[0]);
+    }
+
+    None
 }
 
 fn main() {
     let (rules, my_ticket, nearby_tickets) = read_input("assets/day16.in").unwrap();
+    // let (rules, my_ticket, nearby_tickets) = read_input("assets/example.in").unwrap();
 
     println!("{:?}", rules);
     println!("{:?}", my_ticket);
@@ -92,13 +134,51 @@ fn main() {
 
     // Problem 1
     let mut sum_of_invalid_fields = 0;
-    for ticket in nearby_tickets {
-        for field in ticket.fields {
-            if !is_valid_field(field, &rules) {
+    for ticket in &nearby_tickets {
+        for field in &ticket.fields {
+            if !is_valid_field(*field, &rules) {
                 sum_of_invalid_fields += field;
             }
         }
     }
 
     println!("Sum of troubled fields is {}", sum_of_invalid_fields);
+
+    // Problem 2
+    let mut valid_nearby_tickets = vec![];
+    for ticket in &nearby_tickets {
+        if is_valid_ticket(&ticket, &rules) {
+            valid_nearby_tickets.push(ticket);
+        }
+    }
+
+    let mut remaining_fields: Vec<usize> = (0..rules.len()).collect();
+
+    loop {
+        if remaining_fields.len() == 0 {
+            break
+        }
+
+        for rule in &rules {
+            match learn_rule(&rule, remaining_fields.clone(), &valid_nearby_tickets) {
+                Some(value) => {
+                    println!("Rule {} maps to field {}", rule.name, value);
+                    remaining_fields.retain(|position| *position != value);
+                },
+                None => (),
+            }
+        }
+    }
+
+    // Rule departure time maps to field 2
+    // Rule departure platform maps to field 4
+    // Rule departure station maps to field 8
+    // Rule departure location maps to field 12
+    // Rule departure track maps to field 14
+    // Rule departure date maps to field 19
+
+    // TODO automate the last part.
+    
+    println!("{}", my_ticket.fields[2] * my_ticket.fields[4] * my_ticket.fields[8] * my_ticket.fields[12] * my_ticket.fields[14] * my_ticket.fields[19]);
+
 }
