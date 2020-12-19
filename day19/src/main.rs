2086c684a8c0040ca63c824d1f0f7370cd46c4e9
Roel van Duijnhoven <roel@vanduijnhoven.com>
2020-12-19 14:42:11 +0100

    Solution to part 2.

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -5,11 +5,11 @@ enum Rule {
     Or(Box<Rule>, Box<Rule>),
 }
 
-fn read_input(input: &str) -> (Rule, Vec<String>) {
+fn read_input(input: &str, start_rule: usize) -> (Rule, Vec<String>) {
     let contents = std::fs::read_to_string(input).unwrap();
     let parts: Vec<&str> = contents.split("\n\n").collect();
 
-    let rule = read_rule_tree(parts[0]);
+    let rule = read_rule_tree(parts[0], start_rule);
 
     let messages: Vec<String> = parts[1]
         .split("\n")
@@ -19,13 +19,13 @@ fn read_input(input: &str) -> (Rule, Vec<String>) {
     (rule, messages)
 }
 
-fn read_rule_tree(input: &str) -> Rule {
+fn read_rule_tree(input: &str, start_rule: usize) -> Rule {
     let raw_rules: Vec<String> = input
         .split("\n")
         .map(|item| String::from(&item[item.find(':').unwrap() + 2..]))
         .collect();
 
-    recursive_parse(&raw_rules, &raw_rules[0])
+    recursive_parse(&raw_rules, &raw_rules[start_rule])
 }
 
 // Uses more space than necessary because it will duplicate identical rules in the tree.
@@ -94,20 +94,56 @@ fn valid_message(message: &str, rule: &Rule, start: usize) -> Option<usize> {
     result
 }
 
-fn main() {
-    let (rule, messages) = read_input("assets/simple.in");
-    let (rule, messages) = read_input("assets/example.in");
-    let (rule, messages) = read_input("assets/day19.in");
+// Return vector of tuples.
+// Each tuple (position, times) says until what position we matched already, and
+// how many times we applied the rule.
+fn one_or_more(message: &str, start: usize, rule: &Rule) -> Vec<(usize, usize)> {
+    let mut matches = vec![];
+    let mut times = 0;
+    let mut position = start;
 
-    let mut count = 0;
-    for message in messages {
-        let result = valid_message(&message, &rule, 0);
-        match result {
-            Some(position) if position == message.len() => count += 1,
-            _ => continue,
+    loop {
+        match valid_message(&message, &rule, position) {
+            None => break,
+            Some(value) => {
+                position = value;
+                times += 1;
+                matches.push((value, times));
+            }
         }
     }
 
-    println!("{} valid messages", count);
-        
+    matches
 }
+
+fn main() {
+    // let (rule, messages) = read_input("assets/simple.in");
+    // let (rule, messages) = read_input("assets/example.in");
+
+    // If you think about the starting pattern it will ask you to validate patterns that first repeat rule
+    // 42 one or more time, followed by applying rule 31 one or more times. The extra constraint imposed
+    // is that rule 42 should be called more often than rule 31.
+
+    let (rule_42, _) = read_input("assets/day19.in", 42);
+    let (rule_31, messages) = read_input("assets/day19.in", 31);
+
+    let mut results_matched = 0;
+    for message in messages {
+        let mut matches = false;
+        for (end_position, times_rule_42) in one_or_more(&message, 0, &rule_42) {
+            for (final_position, times_rule_31) in one_or_more(&message, end_position, &rule_31) {
+                if final_position == message.len() && times_rule_42 > times_rule_31 {
+                    matches = true;
+                }
+            }
+        }
+
+        if matches {
+            results_matched += 1;
+            println!("matched {}", message);
+        }
+    }
+
+    println!("{} matched", results_matched);
+}
+ 
