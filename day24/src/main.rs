e856b6b6853d3b51461227479524ff1c35391b90
Roel van Duijnhoven <roel@vanduijnhoven.com>
2020-12-24 08:48:19 +0100

    And part two! Almost there :D.

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs;
 
 fn coordinate(input: &str) -> (isize, isize) {
@@ -31,6 +32,20 @@ fn coordinate(input: &str) -> (isize, isize) {
     (x, y)
 }
 
+fn get_neighbors(coordinate: (isize, isize)) -> Vec<(isize, isize)> {
+    let x = coordinate.0;
+    let y = coordinate.1;
+
+    vec![
+        (x + 1, y - 1),
+        (x, y - 1),
+        (x + 1, y),
+        (x - 1, y),
+        (x, y + 1),
+        (x - 1, y + 1),
+    ]
+}
+
 fn main() {
     let mut is_black: HashMap<(isize, isize), bool> = HashMap::new();
 
@@ -48,12 +63,67 @@ fn main() {
         is_black.insert(coordinate, turn_black);
     }
 
-    let mut black_face_up = 0;
-    for (_, is_black) in is_black {
-        if is_black {
-            black_face_up += 1;
+    // Simulate a day
+    for _ in (0..100) {
+        // Get all possible candidates
+        let mut candidates: Vec<(isize, isize)> = vec![];
+        for (position, _) in &is_black {
+            candidates.push(*position);
+            for neighbor in get_neighbors(*position) {
+                candidates.push(neighbor);
+            }
         }
-    }
 
-    println!("{} have face turned up", black_face_up);
+        // Deduplicate
+        let set: HashSet<_> = candidates.drain(..).collect(); 
+        candidates.extend(set.into_iter());
+
+        // What to flip?
+        let mut flip: Vec<(isize, isize)> = vec![];
+        for position in candidates {
+            let black = match is_black.get(&position) {
+                Some(value) => *value,
+                None => false,
+            };
+
+            let black_neighbor_count = get_neighbors(position)
+                .iter()
+                .map(|neighbor| match is_black.get(neighbor) {
+                    Some(value) => *value,
+                    None => false,
+                })
+                .filter(|black| *black)
+                .count();
+
+            if black && (black_neighbor_count == 0 || black_neighbor_count > 2) {
+                flip.push(position);
+            } 
+
+            if !black && black_neighbor_count == 2 {
+                flip.push(position);
+            }
+        }
+
+        // Now flip
+        for position in flip {
+            let turn_black;
+            if is_black.contains_key(&position) {
+                turn_black = !is_black.get(&position).unwrap();
+            } else {
+                turn_black = true;
+            }
+
+            is_black.insert(position, turn_black);
+        }
+
+        // Report on how many blacks there are
+        let mut black_face_up = 0;
+        for (_, is_black) in &is_black {
+            if *is_black {
+                black_face_up += 1;
+            }
+        }
+
+        println!("{} have face turned up", black_face_up);
+    }
 }
